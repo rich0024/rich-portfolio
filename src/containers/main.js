c3ab6e0f6689aafe79e2fd22f503783ffc7056e1
rich0024 <yance0022@gmail.com>
2021-01-22 15:06:33 -0500

    creating main

---
+++ src/containers/main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class main extends Component {
+    render() {
+        return (
+            <div>
+                <p>main page</p>
+            </div>
+        )
+    }
+}
+
+export default main
