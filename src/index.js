dc572d9ade7667fc04d2999ad6e9cb54adf636b7
rich0024 <yance0022@gmail.com>
2021-01-29 14:55:02 -0500

    styling to index

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from "styled-components";
+import { theme } from "./themes/theme";
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ThemeProvider>,
+  document.getElementById("root")
 );
-
