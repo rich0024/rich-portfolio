f6d2925cf0ec2a6f04f121640f547d36ef339938
rich0024 <yance0022@gmail.com>
2021-02-05 17:47:41 -0500

    media container

---
+++ src/containers/media.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class media extends Component {
+    render() {
+        return (
+            <div>
+                <p>coding tutorials</p>
+            </div>
+        )
+    }
+}
+
+export default media
