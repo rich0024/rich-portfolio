9be8e9eb75ec0a35539a4939b82dfaf755abe715
rich0024 <yance0022@gmail.com>
2021-02-05 17:46:46 -0500

    history container

---
+++ src/containers/work.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class work extends Component {
+    render() {
+        return (
+            <div>
+                <p>made a pizza once</p>
+            </div>
+        )
+    }
+}
+
+export default work
