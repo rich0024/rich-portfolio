d8a63e6d3cbb34db37bceda97c753369da9c82c3
rich0024 <yance0022@gmail.com>
2021-02-05 17:44:29 -0500

    links container

---
+++ src/containers/links.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class links extends Component {
+    render() {
+        return (
+            <div>
+                <p>linkedIn</p>
+            </div>
+        )
+    }
+}
+
+export default links
