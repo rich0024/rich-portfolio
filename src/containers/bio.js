9535d095ac2f2f81fd74b1a0614d5563b9cc730b
rich0024 <yance0022@gmail.com>
2021-02-05 17:43:16 -0500

    about container

---
+++ src/containers/bio.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class bio extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello</p>
+            </div>
+        )
+    }
+}
+
+export default bio
