9586df3965a85047d09d86bfc637f71f84eb9695
rich0024 <yance0022@gmail.com>
2021-02-05 17:45:35 -0500

    projects container

---
+++ src/containers/projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class projects extends Component {
+    render() {
+        return (
+            <div>
+                <p>My Recipe Book</p>
+            </div>
+        )
+    }
+}
+
+export default projects
