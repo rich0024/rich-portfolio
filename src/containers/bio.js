8e6ad36d7c1515bc5f18b287fe69bd3ae5814c57
rich0024 <yance0022@gmail.com>
2021-02-10 13:46:21 -0500

    image

--- src/containers/bio.js
+++ src/containers/bio.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import cropped from './cropped.png'
 
 export class bio extends Component {
     render() {
         return (
             <div>
                 <p>Hello</p>
+                <img src={cropped}>
             </div>
         )
     }
