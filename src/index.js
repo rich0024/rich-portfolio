70e17c63c8a9e4e8977c20bd76013b22be301ac0
rich0024 <yance0022@gmail.com>
2021-02-10 13:00:33 -0500

    style

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { ThemeProvider } from "styled-components";
-import { theme } from "./themes/theme";
+// import { ThemeProvider } from "styled-components";
+// import { theme } from "./themes/theme";
 
 
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
+    <App />,
   document.getElementById("root")
 );
