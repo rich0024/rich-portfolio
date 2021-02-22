acd82ab4f0d8723541c317e31e7ce0c19b4375e9
rich0024 <yance0022@gmail.com>
2021-02-22 18:52:40 -0500

    updated about

--- src/containers/bio.js
+++ src/containers/bio.js
@@ -14,7 +14,7 @@ export class bio extends Component {
 I began my career life as a mortgage loan processor. As I continued processing, I realized that there is nothing
 stopping me from moving up and getting my own loan originator license. As I continued to help people refinance or
 purchase their home, I found that relying on commissions for income was not the most reliable, so I decided to learn 
-a new skill and found The Flatiron School. I immediately realized that I loved coding and the limitless oppurtunities
+a new skill and found The Flatiron School. I immediately realized that I loved coding and the limitless opportunities
 and possibilities it brings. Now, as an eccentric creative software engineer with a background in loan origination, I am 
 passionate about improving and assisting companies, clients and teams with a limitless positive attitude and boundaryless 
 approach to achieving goals, solving problems, and creating efficiencies.
