728f26ec68a5890c3c870d15e819f937f99f47b5
rich0024 <yance0022@gmail.com>
2021-02-22 16:27:17 -0500

    bio and list

--- src/containers/bio.js
+++ src/containers/bio.js
@@ -8,6 +8,13 @@ export class bio extends Component {
             <div>
                 <img src={cropped}></img>
                 <br></br>
+                <h3>About Richard</h3>
+                <br></br>
+                <p>An eccentric creative software engineer with a background in loan origination and a degree in integrated
+mathematics and science (focus in biology) turned software engineer. Passionate about improving and
+assisting companies, clients and teams with a limitless positive attitude and boundaryless approach to
+achieving goals, solving problems, and creating efficiencies.
+</p>
                 <embed src={resume} width="800px" height="1068px"></embed>
             </div>
         )
--- src/containers/projects.js
+++ src/containers/projects.js
@@ -5,7 +5,12 @@ export class projects extends Component {
         return (
             <div>
                 <a href='https://drive.google.com/file/d/1bOysEO40yupx5xljQUtMLGxPuHuwOPKc/view?usp=sharing'>My Recipe Book</a>
-
+                <br></br>
+                <ul>
+                    <li>React based project with user experience in mind which allows for sharing and scrolling of recipes</li>
+                    <li>-SPA with React and Redux frontend, backend built with Ruby on Rails</li>
+                    <li>-Utilized Web Tokens and PostgreSQL to store encrypted user information client-side</li>
+                </ul>
             </div>
         )
     }
