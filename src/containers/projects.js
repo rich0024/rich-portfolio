7886479b530afc4234372888f4302b2d6d6672be
rich0024 <yance0022@gmail.com>
2021-02-22 16:50:28 -0500

    training journal2

--- src/containers/projects.js
+++ src/containers/projects.js
@@ -12,13 +12,24 @@ export class projects extends Component {
                     <li>-Utilized Web Tokens and PostgreSQL to store encrypted user information client-side</li>
                 </ul>
                 <br></br>
-                <a href='https://drive.google.com/file/d/1FXQ9gUdFAMwycyWnFQ60YekbwIzC0JiG/view'>Training Journal</a>
+                <a href='https://drive.google.com/file/d/1FXQ9gUdFAMwycyWnFQ60YekbwIzC0JiG/view'>Training Journal(javascript)</a>
                 <br></br>
                 <ul>
                     <p>SPA which allows the user to create and log a workout routine</p>
                     <li>-Utilized Javascript asynchronicity to create a faster user experience</li>
                     <li>-Implemented a local API and integrated with the front end</li>
                     <li>-Employed multiple event listeners using Fetch to enhance functionality</li>
+                </ul>
+                <br></br>
+                <a href='https://drive.google.com/file/d/1Iae5GWg_UZQ4vHRJ3WDZaidWk6wwb46u/view'>Training Journal(Rails)</a>
+                <br></br>
+                <ul>
+                    <p>A multi-page web application which allows the user to plan and track workout routines.</p>
+                    <li>-Applied omniauth for user login with github</li>
+                    <li>-Maintained MVC structure using Ruby on Rails</li>
+                    <li>-Enabled the user to create, read, update and delete (CRUD) workout routines</li>
+                    <li>-Used foundational code to create classes, class methods, instance methods, while and for loops,
+array iteration, syntax, string interpolation, and object orientation</li>
                 </ul>
             </div>
         )
