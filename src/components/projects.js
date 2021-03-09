169bcad9e4887834c394e37968311a2bb674b17c
rich0024 <yance0022@gmail.com>
2021-03-09 15:48:16 -0500

    projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -7,6 +7,11 @@ class Projects extends React.Component {
   render() {
     return (
       <Desktop3
+      pROJECTS={
+        <>
+          P<br />R<br />O<br />J<br />E<br />C<br />T<br />S
+        </>
+      }
         frame8={Frame1}
         frame7={Frame2}
         frame5={Frame3}
@@ -20,13 +25,16 @@ export default Projects;
 
 class Desktop3 extends React.Component {
   render() {
-    const { frame8, frame7, frame5 } = this.props;
+    const { pROJECTS, frame8, frame7, frame5 } = this.props;
 
     return (
       <div className="desktop-3">
-        <img className="frame-8" src={frame8} />
-        <img className="frame-7" src={frame7} />
-        <img className="frame-5" src={frame5} />
+        <h1 className="p-r-o-j-e-c-t-s mitr-bold-black-48px animate-enter smart-layers-pointers ">{pROJECTS}</h1>
+        <div className="flex-col">
+          <img className="frame-8" src={frame8} />
+          <img className="frame-7" src={frame7} />
+          <img className="frame-5" src={frame5} />
+        </div>
       </div>
     );
   }
