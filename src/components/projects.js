cbb249fb18200b45039b2cb334d491572536296a
rich0024 <yance0022@gmail.com>
2021-03-12 15:07:03 -0500

    demo links

--- src/components/projects.js
+++ src/components/projects.js
@@ -26,14 +26,19 @@ export default Projects;
 class Desktop3 extends React.Component {
   render() {
     const { pROJECTS, frame8, frame7, frame5 } = this.props;
-
     return (
       <div className="desktop-3">
         <h1 className="p-r-o-j-e-c-t-s mitr-bold-black-48px animate-enter smart-layers-pointers ">{pROJECTS}</h1>
         <div className="flex-col">
+          <a href="https://drive.google.com/file/d/1Iae5GWg_UZQ4vHRJ3WDZaidWk6wwb46u/view">
           <img className="frame-8" src={frame8} />
+          </a>
+          <a href="https://drive.google.com/file/d/1bOysEO40yupx5xljQUtMLGxPuHuwOPKc/view">
           <img className="frame-7" src={frame7} />
+          </a>
+          <a href="https://drive.google.com/file/d/1FXQ9gUdFAMwycyWnFQ60YekbwIzC0JiG/view">
           <img className="frame-5" src={frame5} />
+          </a>
         </div>
       </div>
     );
