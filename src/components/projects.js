e479552066789d763acf4b62669472ebd0aa5ffb
rich0024 <yance0022@gmail.com>
2021-03-30 12:37:27 -0400

    projects edit

--- src/components/projects.js
+++ src/components/projects.js
@@ -12,9 +12,6 @@ class Projects extends React.Component {
           P<br />R<br />O<br />J<br />E<br />C<br />T<br />S
         </>
       }
-        frame8={Frame1}
-        frame7={Frame2}
-        frame5={Frame3}
       />
     );
   }
@@ -30,15 +27,29 @@ class Desktop3 extends React.Component {
       <div className="desktop-3">
         <h1 className="p-r-o-j-e-c-t-s mitr-bold-black-48px animate-enter smart-layers-pointers ">{pROJECTS}</h1>
         <div className="flex-col">
+
+          <div className='js-journal'>
+            <h2>Training Journal(javascript)</h2>
           <a href="https://drive.google.com/file/d/1Iae5GWg_UZQ4vHRJ3WDZaidWk6wwb46u/view">
-          <img className="frame-8" src={frame8} />
+          <p>Demo</p>
           </a>
+          <h3>SPA which allows the user to create and log a workout routine</h3>
+          <p>-Utilized Javascript asynchronicity to create a faster user experience
+          -Implemented a local API and integrated with the front end
+          -Employed multiple event listeners using Fetch to enhance functionality</p>
+          </div>
+
+          <div className='recipe'>
           <a href="https://drive.google.com/file/d/1bOysEO40yupx5xljQUtMLGxPuHuwOPKc/view">
           <img className="frame-7" src={frame7} />
           </a>
+          </div>
+
+          <div className='rails-journal'>
           <a href="https://drive.google.com/file/d/1FXQ9gUdFAMwycyWnFQ60YekbwIzC0JiG/view">
           <img className="frame-5" src={frame5} />
           </a>
+          </div>
         </div>
       </div>
     );
