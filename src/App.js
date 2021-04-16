6d135ede1b36bad79c5f8de706d4344155ffa2e1
rich0024 <yance0022@gmail.com>
2021-04-16 01:23:08 -0400

    layout shift

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return(
-      <div>
+    <div className='main'>
       <MainCard />
       <About />
       <Projects />
