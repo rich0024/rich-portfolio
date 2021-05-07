02e30946770df89ea9bf2fda304a994400262847
rich0024 <yance0022@gmail.com>
2021-05-07 14:55:31 -0400

    contact start

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
 
-export default function ContactUs() {
+import React, { Component } from 'react'
 
-  function sendEmail(e) {
-    e.preventDefault();
-
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-  }
-
-  return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <input type="hidden" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
+export class ContactUs extends Component {
+    sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
+    render() {
+        return (
+            <div>
+                <form className="contact-form" onSubmit={sendEmail}>
+                <input type="hidden" name="contact_number" />
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
+                </form>
+            </div>
+        )
+    }
 }
+
+export default ContactUs
